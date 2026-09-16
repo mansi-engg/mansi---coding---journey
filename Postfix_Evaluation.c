f7aec2b46966addb0908f00261dfc2d7fd02233e
Mansi Jain <mj0196106@gmail.com>
2026-09-16 10:41:08 +0530

    Added new Postfix_Evaluation.c code

---
+++ Postfix_Evaluation.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#define MAX 100
+
+int stack[MAX];
+int top = -1;
+
+void push(int x) {
+    stack[++top] = x;
+}
+
+int pop() {
+    return stack[top--];
+}
+
+int main() {
+    char postfix[MAX];
+    int i, a, b, result;
+
+    printf("Enter postfix expression (single-digit operands): ");
+    scanf("%s", postfix);
+
+    for (i = 0; postfix[i] != '\0'; i++) {
+        if (isdigit(postfix[i])) {
+            push(postfix[i] - '0');
+        } else {
+            b = pop();
+            a = pop();
+
+            switch (postfix[i]) {
+                case '+': result = a + b; break;
+                case '-': result = a - b; break;
+                case '*': result = a * b; break;
+                case '/': result = a / b; break;
+                case '%': result = a % b; break;
+                default:
+                    printf("Invalid operator!\n");
+                    return 1;
+            }
+
+            push(result);
+        }
+    }
+
+    printf("Result = %d\n", pop());
+
+    return 0;
+}
