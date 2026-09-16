01cc3d7050d69d76dbafea5e7bd69a15f841fd6e
Mansi Jain <mj0196106@gmail.com>
2026-09-16 10:21:57 +0530

    Added new Double_ll_insertion.c code

---
+++ Double_ll_insertion.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *prev;
+    struct Node *next;
+};
+
+struct Node *insertEnd(struct Node *head, int data) {
+    struct Node *newNode = malloc(sizeof(struct Node));
+
+    newNode->data = data;
+    newNode->next = NULL;
+    newNode->prev = NULL;
+
+    if (head == NULL)
+        return newNode;
+
+    struct Node *temp = head;
+
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = newNode;
+    newNode->prev = temp;
+
+    return head;
+}
+
+void display(struct Node *head) {
+    struct Node *temp = head;
+
+    while (temp != NULL) {
+        printf("%d <-> ", temp->data);
+        temp = temp->next;
+    }
+
+    printf("NULL\n");
+}
+
+int main() {
+    struct Node *head = NULL;
+
+    head = insertEnd(head, 10);
+    head = insertEnd(head, 20);
+    head = insertEnd(head, 30);
+
+    display(head);
+
+    return 0;
+}
